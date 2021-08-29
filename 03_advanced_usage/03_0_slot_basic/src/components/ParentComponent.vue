8727e27f3ded099b02fe798dc2d3a1613ee41fcc
knjtakase <62978135+knjtakase@users.noreply.github.com>
2021-08-29 16:06:06 +0900

    created 03_0_slot_basic

---
+++ 03_advanced_usage/03_0_slot_basic/src/components/ParentComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <ChildComponent>
+    <span>押して：</span>
+    <button @click="onClick">ボタン</button>
+  </ChildComponent>
+</template>
+<script>
+import ChildComponent from "./ChildComponent.vue"
+export default {
+  components: { ChildComponent },
+  methods: {
+    onClick() {
+      alert("押したな！")
+    }
+  }
+}
+</script>
