1bafd4938f885119556bcf7fc8a63194631540f3
knjtakase <62978135+knjtakase@users.noreply.github.com>
2021-09-01 16:56:08 +0900

    create md & mod vue

--- 04_whats_new_in_vue_3/07_fragments/src/components/MyMessage.vue
+++ 04_whats_new_in_vue_3/07_fragments/src/components/MyMessage.vue
@@ -1,11 +1,21 @@
 <template>
-  Hello {{ helloTarget }}!!
+  Hello {{ helloTarget }} !!
+  <div v-for="item in items" :key="item.id">
+    <div>{{ item.data1 }}</div>
+    <div>{{ item.data2 }}</div>
+    <div>{{ item.data3 }}</div>
+  </div>
 </template>
 <script>
 export default {
-  data () {
+  data() {
     return {
-      helloTarget: 'World',
+      helloTarget: "World",
+      items: [
+        { id: 1, data1: "dataOne", data2: "dataTwo", data3: "dataThree" },
+        { id: 2, data1: "dataOne", data2: "dataTwo", data3: "dataThree" },
+        { id: 3, data1: "dataOne", data2: "dataTwo", data3: "dataThree" }
+      ]
     }
   }
 }
