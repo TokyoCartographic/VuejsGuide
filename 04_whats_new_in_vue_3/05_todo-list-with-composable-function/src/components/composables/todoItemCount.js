4bdc4089e009520e3e97f421649ee83209e143e9
knjtakase <62978135+knjtakase@users.noreply.github.com>
2021-09-05 16:54:50 +0900

    mod js comment

--- 04_whats_new_in_vue_3/05_todo-list-with-composable-function/src/components/composables/todoItemCount.js
+++ 04_whats_new_in_vue_3/05_todo-list-with-composable-function/src/components/composables/todoItemCount.js
@@ -2,7 +2,7 @@ import { computed } from "vue"
 /**
  * 現在のToDO配列の数を返す
  * @param { Array } items 元になる配列
- * @return {Number} itemCount 配列数
+ * @return {Number} itemCount 配列数refオブジェクト
  */
 export const todoItemCount = (items) => {
   return computed(() => {
